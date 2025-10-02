70f81330974040e8dbaf0f186d5a3b15ebf852bf
BelinaJaroslav <jaroslav.belina@tul.cz>
2025-10-02 09:02:52 +0200

    Task 03 refactor + task 04

--- App/App.cpp
+++ App/App.cpp
@@ -13,6 +13,22 @@ bool App::Init()
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_WARNING);
 
     //TODO: Open capture device (camera or file) stream here, store capture device variable as protected member variable in App class. If camera is not found, open the video file.
+    //capture.open(0);
+
+    if (!capture.isOpened()) {
+        fmt::println("No camera found, trying fallback video file...");
+        capture.open("App/Resources/video.mkv");
+    }
+
+    if (!capture.isOpened()) {
+        std::cerr << "Error: Could not open camera or video file." << std::endl;
+        return false;
+    }
+
+    fmt::println("Initialized capture device. Width: {}, Height: {}",
+        capture.get(cv::CAP_PROP_FRAME_WIDTH),
+        capture.get(cv::CAP_PROP_FRAME_HEIGHT));
+
 
     fmt::println("Initialized.");
     return true;
@@ -25,9 +41,9 @@ void App::Run()
     // Lab 01 Task 02
     //FindRedObjectInImage();
     // Lab 01 Task 03
-    FindRedObjectInVideo();
+    //FindRedObjectInVideo();
     // Lab 01 Task 04
-    //FindFaceInVideo();
+    FindFaceInVideo();
 }
 
 App::~App()
