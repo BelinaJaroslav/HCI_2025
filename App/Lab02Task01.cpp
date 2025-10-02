ebd53193b4b6638057743a0f45cbd5744395d337
RadekMocek <radek.mocek@tul.cz>
2025-10-02 23:32:46 +0200

    Lab02 done

--- App/Lab02Task01.cpp
+++ App/Lab02Task01.cpp
@@ -4,6 +4,9 @@
 
 void App::lab_complex_behaviour()
 {
+    cv::Mat image_no_face = cv::imread("App/Resources/looking_for_user.jpg");
+    cv::Mat image_warning = cv::imread("App/Resources/warning.jpg");
+
     cv::Mat frame; // For captured frame & for displaying
 
     do {
@@ -14,14 +17,34 @@ void App::lab_complex_behaviour()
             break;
         }
 
-        // Find face
-        cv::Point2f center(0);
+        // Find faces
+        auto face_centers = face_detector.find_faces(frame);
+        auto n_faces_found = face_centers.size();
 
-        // Draw crosses
-        draw_cross_normalized(frame, center, 30, CV_RGB(0, 200, 255));
-        
-        // Display result
-        cv::imshow("Face Detection", frame);
+        // Act depending on number of faces found
+        if (n_faces_found == 0) {
+            // "Splashscreen"
+            cv::imshow("Face Detection", image_no_face);
+        }
+        else if (n_faces_found == 1) {
+            // Find red object and draw cross
+            auto red_object_center = find_red_object_chroma(frame);
+            draw_cross_normalized(frame, red_object_center, 30, CV_RGB(0, 200, 255)); // blue cross
+
+            // Draw face crosses
+            /*
+            for (const auto& face_center : face_centers) {
+                draw_cross_normalized(frame, face_center, 30, CV_RGB(203, 0, 248)); // pink cross
+            }
+            /**/
+
+            // Display result
+            cv::imshow("Face Detection", frame);
+        }
+        else {
+            // "Lockscreen"
+            cv::imshow("Face Detection", image_warning);
+        }        
 
         // Measure FPS
         if (fps_meter.is_updated()) fmt::println("FPS: {:.3f}", fps_meter.get());
